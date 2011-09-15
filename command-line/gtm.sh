8511dadb303566b7ebf3e51d75ecc5ae84cdc87e
hborders <hborders@mail.win.org>
2011-09-15 00:27:24 -0500

    Examples of running tests from the command line

---
+++ command-line/gtm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+PROJECT_DIR=`cd $(dirname $0)/../DiceGame; pwd`
+
+cd $PROJECT_DIR
+xcodebuild -target GTMUnitTest -sdk iphonesimulator -configuration Debug
---
+++ command-line/ocunit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+PROJECT_DIR=`cd $(dirname $0)/../DiceGame; pwd`
+
+xcodebuild -workspace $PROJECT_DIR/DiceGame.xcodeproj/project.xcworkspace -scheme BuiltInLogicUnitTest -sdk iphonesimulator
