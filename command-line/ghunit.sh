71c9951327d057e9ea0dc00bce79912fd7c8fcfc
hborders <hborders@mail.win.org>
2011-09-15 01:19:25 -0500

    Added GHUnit command line tests.

---
+++ command-line/ghunit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+PROJECT_DIR=`cd $(dirname $0)/../DiceGame; pwd`
+
+cd $PROJECT_DIR
+GHUNIT_CLI=1 WRITE_JUNIT_XML=YES xcodebuild -target GHUnitTest -configuration Debug -sdk iphonesimulator build
